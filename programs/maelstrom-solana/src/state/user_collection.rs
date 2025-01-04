fcf23c54fec4b5fa16c88710ddfe43d2c2a9ed46
francisy9 <francisyang@uchicago.edu>
2025-01-04 14:05:28 +0100

    added explanation in state

--- programs/maelstrom-solana/src/state/user_collection.rs
+++ programs/maelstrom-solana/src/state/user_collection.rs
@@ -4,6 +4,10 @@ use anchor_lang::prelude::*;
 pub struct UserCollection {
     pub collection_id: u8,
     pub gold: u16,
+    // [16, 0...] means 1 count of cardid 0
+    // bit rep 0001 0000
+    // [1, 0...] means 1 count of cardid 1
+    // bit rep 0000 0001
     pub card_count: [u8; 100],
     pub bump: u8,
 }
